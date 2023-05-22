1d2f8758d9fa0f9bc8ff0e54804e94ea0fbe8b1c
ENKI <juan.avalon4@gmail.com>
2023-05-22 12:10:14 -0600

    titles

--- components/FeaturedPostCard.jsx
+++ components/FeaturedPostCard.jsx
@@ -12,10 +12,10 @@ const FeaturedPostCard = ({ post }) => (
  <div className="absolute rounded-full bg-center  bg-gradient-to-b opacity-40 from-gray-400 via-gray-800 to-black w-full h-60 bg-ble nd-multiply" />
 
      <div className="flex flex-col items-center absolute bottom-5 w-full justify-center">
-     <p className='text-white ease-in-out duration-300 group/edit opacity-0 group-hover/item:opacity-100 font-["monument"] '>VIEW NOW</p>
+     <p className='text-white ease-in-out duration-300 group/edit opacity-0 group-hover/item:opacity-100 font-["monument"]'>VIEW NOW</p>
     
-        <p className="inline align-middle uppercase font-sans  text-white text-shadow ml-2 text-xs">{post.title}</p>
-        <p className="text-white align.  mb-4 text-shadow font-semibold text-xs">{moment(post.createdAt).format('MMM DD, YY')}</p>
+        <p className="inline text-center uppercase font-sans  text-white text-shadow ml-2 text-xs">{post.title}</p>
+        <p className="text-white text-center mb-4 text-shadow font-semibold text-xs">{moment(post.createdAt).format('MMM DD, YY')}</p>
       </div>
 
       <Link href={`/post/${post.slug}`}><span className="cursor-pointer absolute w-full h-full" /></Link>
